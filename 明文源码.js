dd0ca1bfafe749c06374a69b2e6367e28e68651f
phoenixa2012 <132251134+phoenixa2012@users.noreply.github.com>
2025-01-29 11:38:15 +0330

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -1,7 +1,7 @@
 addEventListener(
   "fetch", event => {
   let url = new URL(event.request.url);
-  url.hostname = "s6xui.speedyflare.ir";
+  url.hostname = "s7xui.speedyflare.ir";
   url.protocol = "https";
   let request = new Request(url, event.request);
   event.respondWith(fetch(request)
